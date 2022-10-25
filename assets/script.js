90ce799911bf5782365a5e6cce7b46dfad9b7981
Sylvia Ely <sylvianne9417@gmail.com>
2022-10-24 17:53:02 -0700

    working on basic psuedocode

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,29 @@
 /*observations from CSS
 
 - We'll need:
-1) To Add Moment
+
+1) Display the current day is displayed at the top of the calendar
+    Connect Moment () to  <p id="currentDay" class="lead"></p>
+2) THEN I am presented with timeblocks for standard business hours 9am - 5pm
+
+timeblock container
+timeblock form??? input??? 
+save button to the right? class = ".saveBtn"
+
+3) Code Timeblocks so that they indicate whether it is in the past, present, or future
+
+class = ".past" ".present"  ".future"
+
+probably have to find current time and compare it to the time for each form.  
+
+4) WHEN I click into a timeblock, THEN I can enter an event
+
+form.
+
+5) WHEN I click the save button for that timeblock THEN the text for that event is saved in local storage
+
+json connect
 
 
-
-
-
-
-
-
+6) WHEN I refresh the page THEN the saved events persist
 */
